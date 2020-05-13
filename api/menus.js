b0b93fd3b1d1fba180e3661fa6c963858ef75c6c
sinyak0vsky <sinyak0vsky@gmail.com>
2020-05-13 15:46:13 +0300

    Add PUT $ Delete /api/menus/:menuId

--- api/menus.js
+++ api/menus.js
@@ -27,6 +27,21 @@ menusRouter.get('/', (req, res, next) => {
 });
 
 // POST api/menus
+menusRouter.post('/', (req, res, next) => {
+  const {title} = req.body.menu;
+  if (!title) {
+    return res.status(400).send();
+  }  
+
+  db.run('INSERT INTO Menu (title) VALUES ($title) ', {$title: title}, function(err) {
+    if (err) return next(err);
+
+    db.get('SELECT * FROM Menu WHERE id = $id', {$id: this.lastID}, (err, row) => {
+      if (err) return next(err);
+      res.status(201).send({menu: row});
+    });
+  });
+});
 
 // GET api/menus/:menuId
 menusRouter.get('/:menuId', (req, res, next) => {
@@ -34,6 +49,37 @@ menusRouter.get('/:menuId', (req, res, next) => {
   db.get('SELECT * FROM Menu WHERE id = $menuId', {$menuId: menuId}, (err, row) => {
     if (err) return next(err);
     res.status(200).send({menu: row});
+  });  
+});
+
+// PUT api/menus/:menuId
+menusRouter.put('/:menuId', (req, res, next) => {
+  const menuId = req.params.menuId;
+  const {title} = req.body.menu;
+  if (!title) {
+    return res.status(400).send();
+  }  
+  db.run('UPDATE Menu SET title = $title WHERE id = $menuId', {$title: title, $menuId: menuId}, function(err) {
+    if (err) return next(err);
+    db.get('SELECT * FROM Menu WHERE id = $menuId', {$menuId: menuId}, (err, row) => {
+      if (err) return next(err);
+      res.status(200).send({menu: row});
+    });
+  });
+});
+
+// DELETE api/menus/:menuId
+menusRouter.delete('/:menuId', (req, res, next) => {
+  const menuId = req.params.menuId;
+  db.get('SELECT * FROM MenuItem WHERE menu_id = $menuId', {$menuId: menuId}, (err, row) => {
+    if (err) return next(err);
+    if (row) {
+      return res.status(400).send();
+    }
+    db.run('DELETE FROM Menu WHERE id = $menuId', {$menuId: menuId}, function(err) {
+      if (err) return next(err);
+      res.status(204).send();
+    });
   });  
 });
 
